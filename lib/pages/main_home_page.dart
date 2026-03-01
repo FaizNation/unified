7d38d3e4421e66ab81719b4f86a213a95a7ac689
FaizNation <novaatalagrab@gmail.com>
2026-03-01 08:19:06 +0700

    fix bug current user on home

--- lib/pages/main_home_page.dart
+++ lib/pages/main_home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MainHomePage extends StatefulWidget {
   const MainHomePage({super.key});
@@ -20,11 +22,37 @@ class _MainHomePageState extends State<MainHomePage> {
     _loadData();
   }
 
-  void _loadData() async {
+  Future<void> _loadData() async {
     final prefs = await SharedPreferences.getInstance();
+    
+    // Load local phase preference
     setState(() {
       _phase = prefs.getString('phase');
     });
+
+    // Load user data from Firestore
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      try {
+        final doc = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+        if (doc.exists && mounted) {
+          final data = doc.data()!;
+          setState(() {
+            _currentUser = data['name'] ?? user.displayName ?? "User";
+          });
+        } else if (mounted) {
+           setState(() {
+             _currentUser = user.email?.split('@')[0] ?? "User";
+           });
+        }
+      } catch (e) {
+        if (mounted) {
+           setState(() {
+             _currentUser = user.email?.split('@')[0] ?? "User";
+           });
+        }
+      }
+    }
   }
 
   String _getGreeting() {
